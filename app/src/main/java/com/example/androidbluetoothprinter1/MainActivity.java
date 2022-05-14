99736d7fe5e9cc9fe1d3b26c3c13c40508a0d4af
Donny Malik Kurniawan <bossdony@gmail.com>
2022-05-14 15:55:23 +0700

    Sudah bisa print gambar Selanjutnya perlu dilakukan scaling image agar ketika diprint tidak terpotong karena oversize terhadap kertas thermal.

--- app/src/main/java/com/example/androidbluetoothprinter1/MainActivity.java
+++ app/src/main/java/com/example/androidbluetoothprinter1/MainActivity.java
@@ -6,10 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -28,6 +35,7 @@ import com.mazenrashed.printooth.utilities.PrintingCallback;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements PrintingCallback {
@@ -37,6 +45,9 @@ public class MainActivity extends AppCompatActivity implements PrintingCallback 
 
     Printing printing;
     Button btn_unpair_pair, btn_print, btn_print_image;
+
+    // inisialisasi data Bitmap
+    Bitmap bmNormal, bmGrayScale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements PrintingCallback 
                 startActivityForResult(new Intent(MainActivity.this,
                                 ScanningActivity.class),
                         ScanningActivity.SCANNING_FOR_PRINTER);
-                printText();
+                //printText();
                 printImages();
         });
 
@@ -120,14 +131,35 @@ public class MainActivity extends AppCompatActivity implements PrintingCallback 
         Printooth.INSTANCE.printer().print(printables);
     }
 
-    // tampilkan gambar (untuk kebutuhan uji coba)
+    // tampilkan gambar di image view (untuk pertama kali)
     private void embedImages() {
         ArrayList<Printable> printables = new ArrayList<>();
 
         //Load image
         Picasso.get()
-                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/512px-Gmail_icon_%282020%29.svg.png")
-                .into(imgGallery);
+                //.load("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/512px-Gmail_icon_%282020%29.svg.png")
+                .load(R.drawable.foldedhand)
+                .resize(400, 400)
+                .centerInside()
+                //.into(imgGallery);
+                .into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        //printables.add(new ImagePrintable.Builder(bitmap).build());
+                        bmGrayScale = toGrayscale(bitmap);
+                        imgGallery.setImageBitmap(bmGrayScale);
+                    }
+
+                    @Override
+                    public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+                    }
+                });
     }
 
     // cetak gambar
@@ -135,12 +167,18 @@ public class MainActivity extends AppCompatActivity implements PrintingCallback 
         ArrayList<Printable> printables = new ArrayList<>();
 
         //Load image
-        Picasso.get()
-                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/512px-Gmail_icon_%282020%29.svg.png")
+/*        Picasso.get()
+                //.load("https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Gmail_icon_%282020%29.svg/512px-Gmail_icon_%282020%29.svg.png")
+                .load(R.drawable.foldedhand)
+                .resize(400, 400)
+                .centerInside()
                 .into(new Target() {
                     @Override
                     public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                        printables.add(new ImagePrintable.Builder(bitmap).build());
+                        //printables.add(new ImagePrintable.Builder(bitmap).build());
+                        printables.add(new ImagePrintable.Builder(toGrayscale(bitmap))
+                                .setAlignment(DefaultPrinter.Companion.getALIGNMENT_CENTER())
+                                .build());
 
                         //printing.print(printables);
                         Printooth.INSTANCE.printer().print(printables);
@@ -155,7 +193,25 @@ public class MainActivity extends AppCompatActivity implements PrintingCallback 
                     public void onPrepareLoad(Drawable placeHolderDrawable) {
 
                     }
-                });
+                });*/
+        printables.add(new ImagePrintable.Builder(bmGrayScale).build());
+        Printooth.INSTANCE.printer().print(printables);
+    }
+
+    // sumber https://stackoverflow.com/questions/58791821/printooth-is-not-printing-the-image
+    public static Bitmap toGrayscale(Bitmap srcImage) {
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(srcImage.getWidth(),
+                srcImage.getHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+        canvas.drawBitmap(srcImage, 0, 0, paint);
+        return bmpGrayscale;
     }
 
     // perintah print ke device
@@ -216,11 +272,30 @@ public class MainActivity extends AppCompatActivity implements PrintingCallback 
         changePairAndUnpair();
 
         // membaca hasil perintah load gambar dari galeri
+        // referensi http://android-er.blogspot.com/2015/11/convert-bitmap-to-grayscale-using.html
         if (resultCode == RESULT_OK) {
             if (requestCode == GALLERY_REQ_CODE) {
                 // for gallery
-
-                imgGallery.setImageURI(data.getData());
+                //imgGallery.setImageURI(data.getData());
+                Uri dataUri = data.getData();
+                int w = imgGallery.getWidth();
+                int h = imgGallery.getHeight();
+                Toast.makeText(MainActivity.this,
+                        dataUri.toString() + "\n" +
+                                w + " : " + h,
+                        Toast.LENGTH_LONG).show();
+                try {
+                    bmNormal = bmGrayScale = null;
+                    bmNormal = loadScaledBitmap(dataUri, w, h);
+                    bmGrayScale = toGrayscale(bmNormal);
+                    imgGallery.setImageBitmap(bmGrayScale);
+                    Toast.makeText(MainActivity.this,
+                            bmGrayScale.getWidth() + " x " + bmGrayScale.getHeight(),
+                            Toast.LENGTH_SHORT).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
             }
         }
 
@@ -229,5 +304,56 @@ public class MainActivity extends AppCompatActivity implements PrintingCallback 
     @Override
     public void disconnected() {
 
+    }
+
+    /*
+reference:
+Load scaled bitmap
+http://android-er.blogspot.com/2013/08/load-scaled-bitmap.html
+ */
+    private Bitmap loadScaledBitmap(Uri src, int req_w, int req_h) throws FileNotFoundException {
+
+        Bitmap bm = null;
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(getBaseContext().getContentResolver().openInputStream(src),
+                null, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, req_w, req_h);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        bm = BitmapFactory.decodeStream(
+                getBaseContext().getContentResolver().openInputStream(src), null, options);
+
+        return bm;
+    }
+
+    public int calculateInSampleSize(BitmapFactory.Options options,
+                                     int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and
+            // width
+            final int heightRatio = Math.round((float) height
+                    / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will
+            // guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
     }
 }
